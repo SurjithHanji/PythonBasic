a76d878795d36b5e5bacb551d17685499c3c0da2
SurjithHanji <surjithhanji12345@gmail.com>
2025-01-01 19:09:50 +0530

    Add files via upload

---
+++ python_harry/first.py
@@ -0,0 +1,23 @@
+a=10
+print(a)
+b="harry"
+harry=9
+print(b)
+d=1.23
+print(type(b))
+c=True
+print(type(c))
+print("the type of c is:",type(c))
+print("the type of d is:",type(d))
+
+
+# list is collection of elemnts of different data types
+
+l=[1,2,3,4,[55,54,34]]
+print(l)
+print(l[0])
+
+# dictionary is collecion of key value pairs
+
+dict={"name":"surjith","age":21}
+print(dict)
---
+++ python_harry/typecaste.py
@@ -0,0 +1,10 @@
+a="1"
+b="2"
+print(int(a)+int(b))
+
+
+#implicit type casting
+
+c=1.9
+d=2
+print(c+d)
