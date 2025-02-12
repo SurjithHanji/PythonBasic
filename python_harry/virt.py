a590d959a612b4175566ae1f718f77b63f9ec4e2
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-12 22:13:17 +0530

    Add files via upload

---
+++ python_harry/virt.py
@@ -0,0 +1,18 @@
+from math import sqrt,pi
+print(sqrt(78)*pi)
+
+import math
+math.floor(4.23345)
+print(math.sqrt(25))
+
+
+
+from math import *
+#all functions are imported
+# 
+
+from math import sqrt as s
+print(s(100))
+
+import math
+print(dir(math))
