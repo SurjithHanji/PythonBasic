21d73f2eea5aba2b16f67e79e6314d48c3ece368
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-13 20:29:33 +0530

    Add files via upload

---
+++ WsCubeTech_Smpl_problems/area.py
@@ -0,0 +1,3 @@
+from math import pi
+r=float(input("enter radius of a circle:"))
+print(pi*r*r)
--- WsCubeTech_Smpl_problems/fact.py
+++ WsCubeTech_Smpl_problems/fact.py
@@ -1,11 +1,12 @@
 def fact(n):
-    if n==1:
+    if n<=1:
         return n
     else:
         return n*fact(n-1)
-    
-n=int(input("value for n:"))
-if(n<=0):
-    print("factorial is not exist for n<=0")
-else:
-    print("factorial of number is:",fact(n))
+n=7 
+print(fact(7))
+
+import math
+print(math.factorial(n))
+n=7
+print("factorial of",n,"is",fact(n))
