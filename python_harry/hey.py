30862ba5af4403cd308491a5aaddbd95084e89c8
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-19 22:45:11 +0530

    Add files via upload

---
+++ python_harry/hey.py
@@ -0,0 +1,10 @@
+n=10
+
+def fun():
+    global n
+    n=4
+    y=5
+    print(y)
+
+fun()
+print(n)
---
+++ python_harry/hlo.py
@@ -0,0 +1,12 @@
+x=4
+print(x)
+
+def hello():
+    x=5
+    
+    print(f"the local x is {x}")
+    print(x)
+    print('hlo harry')
+
+hello()
+print(f"the global x is{x} ")
