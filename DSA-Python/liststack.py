d3af62628f516e15ae756800a6ccd9a75e1d07a9
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-20 22:05:37 +0530

    Add files via upload

---
+++ DSA-Python/liststack.py
@@ -0,0 +1,13 @@
+#implementation using list
+
+stack=[]
+stack.append("welcome")
+stack.append("to")
+stack.append("great learning")
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
