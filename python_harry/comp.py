e77dc37361e947e76e298a54861c033b77c31367
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-23 22:15:35 +0530

    Add files via upload

---
+++ python_harry/comp.py
@@ -0,0 +1,16 @@
+a=4
+e="4"
+
+b=[1,2,343]
+c=[1,3,45]
+
+d=3
+f=3
+print(d is f)
+print(d==f)
+
+print(b is c)
+print(b==c)
+
+print(a is e)#exact locationn of object in memory
+print(a==e)#compares a value
