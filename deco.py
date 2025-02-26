9aaf5354cbcfa6e52b82244099681b60059f414e
SurjithHanji <surjithhanji12345@gmail.com>
2025-02-26 19:06:11 +0530

    Add files via upload

---
+++ deco.py
@@ -0,0 +1,15 @@
+def greet(fx):
+    def mfx():
+        print("good morning")
+        fx()
+        print("thanks for using this function")
+    return mfx
+
+@greet
+def hello():
+    print("hello world")
+
+def add(a,b):
+    print(a+b)
+
+hello()
