e9f1eefc9064797a063c9dcd454af300fce8f25f
SurjithHanji <surjithhanji12345@gmail.com>
2025-03-01 23:56:34 +0530

    Add files via upload

--- python_harry/snake.py
+++ python_harry/snake.py
@@ -1,27 +1,15 @@
-import random
+class Math:
+    def __init__(self,num):
+        self.num=num
 
+    def addtonum(self,n):
+        self.num=self.num+n
 
-def check(compu,user):
-    if(compu==user):
-        print("its draw")
-    if(compu==0 and user==1):
-        return -1
-    if(compu==1 and user==2):
-        return -1
-    if(compu==2 and user==0):
-        return -1
-    return 1
-
-compu=random.randint(0,2)
-user=int(input("0 for snake, 1 for water and 2 for gun"))
-
-print("user:",user) 
-print("computer:",compu)
-
-score=check(compu,user)
-if(score==0):
-    print("its draw")
-elif(score==-1):
-    print("you lose")
-else:
-    print("you won")
+    @staticmethod
+    def add(a,b):
+        return a+b
+    
+a=Math(5)
+print(a.num)
+a.addtonum(6)
+print(a.num)
